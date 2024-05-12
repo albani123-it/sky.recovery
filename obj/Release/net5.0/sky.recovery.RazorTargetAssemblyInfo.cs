048c521a7c3fbfd00ed8d12480adcd7667f89876
tossasyahlevi01 <tossasyahlevi01@gmail.com>
2024-05-12 12:07:17 +0700

    up

--- obj/Release/net5.0/sky.recovery.RazorTargetAssemblyInfo.cs
+++ obj/Release/net5.0/sky.recovery.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyProductAttribute("sky.recovery")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95df2e839509d9c4d922d74f53d3193b0c0487c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52b760dfbbddc6736c9b5ce6ac65db98e6eeba07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sky.recovery.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
