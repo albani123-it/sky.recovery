2617a45da0f4deb4320c50da6a7b3e40ad69deb2
tossasyahlevi01 <65279752+tossasyahlevi01@users.noreply.github.com>
2024-06-01 12:55:31 +0700

    up

--- obj/Release/net5.0/sky.recovery.AssemblyInfo.cs
+++ obj/Release/net5.0/sky.recovery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sky.recovery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d42de6babac1ce8ba2d52fbfbc778782720d7fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+552299df498eea622dbb1ee048cec59a459e1892")]
 [assembly: System.Reflection.AssemblyProductAttribute("sky.recovery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sky.recovery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
